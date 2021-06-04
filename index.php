748813a62d319a58c7d118c7ef96238ead4d1373
tanaphoom <fovin14@gmail.com>
2021-06-04 15:45:47 +0700

    boot

--- index.php
+++ index.php
@@ -4,6 +4,15 @@
 <title>oooooo</title>
 </hash>
 <body>
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand">Navbar</a>
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
 
     <?php
 
