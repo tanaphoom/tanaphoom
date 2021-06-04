79b7a13f1543b33099f4a37c30e0364c999504ed
tanaphoom <fovin14@gmail.com>
2021-06-04 16:51:40 +0700

    oif

--- index.php
+++ index.php
@@ -30,13 +30,13 @@
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="l1.jpg"  width="50%">
+      <img src="l1.jpg"  width="100%">
     </div>
     <div class="carousel-item">
-      <img src="l2.jpg" width="50%">
+      <img src="l2.jpg" width="100%">
     </div>
     <div class="carousel-item">
-      <img src="l3.jpg" width="50%">
+      <img src="l3.jpg" width="100%">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
