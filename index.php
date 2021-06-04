4631d26290e2c17bb7fe90816cfa6fee0029d859
tanaphoom <fovin14@gmail.com>
2021-06-04 15:32:18 +0700

    dff

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html>
+<hash>
+</hash>
+<body>
 
-echo "Hello World";
-echo "lllllllll";
+    <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+        <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
+        Bootstrap
+        </a>
+    </div>
+    </nav>
+                
 
-?>
+
+</body>
+</html>
