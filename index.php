f39256b80ff49f39d6e23bec68faa96d1515792d
tanaphoom <fovin14@gmail.com>
2021-06-07 11:07:16 +0700

    oo

--- index.php
+++ index.php
@@ -50,7 +50,15 @@
   </button>
 </div>
 
-<link rel="profile" href="web.php">
+
+<div class="card mb-3">
+  <img src="l3.jpg" class="card-img-top" alt="500px">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+</div>
 
 
     
