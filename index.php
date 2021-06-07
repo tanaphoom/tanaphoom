9a8b8e4866f2808c588d56028f4e146ff58cd0b7
tanaphoom <fovin14@gmail.com>
2021-06-07 11:04:15 +0700

    dd

--- index.php
+++ index.php
@@ -50,21 +50,9 @@
   </button>
 </div>
 
+<link rel="profile" href="web.php">
 
-<div class="card mb-3" style="max-width: 540px;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="l2.jpg" alt="700px" vspace="100">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text" vspace="100">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-  </div>
-</div>
+
     
                 
     
--- web.php
+++ web.php
@@ -1,7 +1,38 @@
-<?php
-include 'index.php';
-include 'show.php';
+
+
+<!DOCTYPE html>
+<html lang="en">
+<hash>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="wiodth=device-width, initial-scale=1.0">
+    <title>learn bootstrapb </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    
+
+</hash>
+<body>
 
 
 
-?>
+<div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="l2.jpg" alt="700px" vspace="100">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text" vspace="100">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+    
+                
+    
+                
+            
+</body>
+</html>
