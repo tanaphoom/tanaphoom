ea705c63c8e7c195b4fd67ca04fe58c3d22c9271
tanaphoom <fovin14@gmail.com>
2021-06-07 11:33:02 +0700

    sss

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
 <div class="card mb-3" style="max-width: 540px;">
   <div class="row g-0">
     <div class="col-md-4">
-      <img src="l3.jpg" while="50%">
+      <img src="l3.jpg" while="500px" vspace="30">
     </div>
     <div class="col-md-8">
       <div class="card-body">
