4dfd58d53d4625a82b0426cdafb2d66fb80c47c8
tanaphoom <fovin14@gmail.com>
2021-06-07 11:46:57 +0700

    oooo

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
 <br>
 <br>
 
-<div class="card mb-3" style="max-width: 540px;">
+<div class="card-group" style="max-width: 540px;">
   <div class="row g-0">
       <img src="l3.jpg" while="500px">
     </div>
@@ -67,7 +67,19 @@
   </div>
 </div>
    
-                
+<div class="card" style="max-width: 540px;">
+  <div class="row g-0">
+      <img src="l3.jpg" while="500px">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div> 
     
                 
             
